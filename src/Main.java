9d3d10577690e7be761b5e34fbca5cfc84e122f7
mdrag <m.dragay@yandex.ru>
2023-01-03 22:31:20 +0300

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
 
     }
 
@@ -59,6 +60,36 @@ public class Main {
         }
         else {
             System.out.println( "Свыше 100 км доставки нет.");
+        }
+    }
+    public static void task5(){
+        System.out.println("Задача 5");
+        int monthNumber = 8;
+        switch (monthNumber) {
+            case 1: System.out.println("Январь");
+                break; case 2:
+                System.out.println("Февраль");
+                break; case 3:
+                System.out.println("Март");
+                break; case 4:
+                System.out.println("Апрель");
+                break; case 5:
+                System.out.println("Май");
+                break; case 6:
+                System.out.println("Июнь");
+                break; case 7:
+                System.out.println("Июль");
+                break; case 8:
+                System.out.println("Август");
+                break; case 9:
+                System.out.println("Сентябрь");
+                break; case 10:
+                System.out.println("Октябрь");
+                break; case 11:
+                System.out.println("Ноябрь");
+                break; case 12:
+                System.out.println("Декабрь");
+                break; default:
         }
     }
 }
