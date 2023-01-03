20f44bf271a149546c4a53914ff9586e17321496
mdrag <m.dragay@yandex.ru>
2023-01-03 22:26:31 +0300

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
 
     }
 
@@ -42,6 +43,22 @@ public class Main {
             System.out.println(year + " год високосный");
         } else {
             System.out.println(year + " год не високосный");
+        }
+    }
+    public static void task4(){
+        System.out.println("Задача 4");
+        int deliveryDistance = 60;
+        if (deliveryDistance <= 20) {
+            System.out.println( "Потребуется 1 день ");
+        }
+        else if (deliveryDistance >20 && deliveryDistance <= 60){
+            System.out.println( "Потребуется 2 дня ");
+        }
+        else if (deliveryDistance >60 && deliveryDistance <= 100) {
+            System.out.println( "Потребуется 3 дня ");
+        }
+        else {
+            System.out.println( "Свыше 100 км доставки нет.");
         }
     }
 }
