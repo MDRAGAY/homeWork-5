74a45e69e5e7375686a9faea4c4a079cc2e4bf73
mdrag <m.dragay@yandex.ru>
2023-01-03 21:42:58 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача1");
+        byte clientOS = 0;
+        if (clientOS == 1) {
+            System.out.println("Установите версию для Android по ссылке");
+        } else {
+            System.out.println("Установите версию для IOS по ссылке");
+        }
     }
 }
